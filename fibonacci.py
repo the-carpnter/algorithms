728a466b1e3d9acd53e20d89f4533eeff89ed592
Anupriyo Mandal <anupriyomandal@Anupriyos-MacBook-Pro.local>
2020-11-16 14:26:14 +0530

    fibonacci added

---
+++ fibonacci.py
@@ -0,0 +1,19 @@
+# This is head recursion
+def fibonacci(n):
+    if n in {0,1}:
+        return n
+    fib_1 = fibonacci(n-1)
+    fib_2 = fibonacci(n-2)
+    result = fib_1 + fib_2
+    return result
+
+# This is tail recursion
+def fib(n, a=0, b=1):
+    if n == 0: return a
+    if n == 1: return b
+    return fib(n-1, b, a+b)
+
+
+if __name__ == '__main__':
+    print(fibonacci(10))
+    print(fib(10))
